7eb7536d4024cfa95d4ddb39c15bc1d39c34b42b
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-10 12:01:52 +0530

    Add files via upload

---
+++ S1.java
@@ -0,0 +1,8 @@
+public class S1{
+    public static void main(String args[]){
+    String a="hello";
+    String b= new String("hai");
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
