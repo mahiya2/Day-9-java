c7c790ae18b61d6b16f99cc9f602f130dce96de7
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-10 13:56:46 +0530

    Add files via upload

---
+++ Splitsentence.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Splitsentence{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        //splitting a sentence
+        String a=sc.nextLine();
+        String sp[]=a.split(" ");
+        System.out.println(Arrays.toString(sp));
+    }
+}
